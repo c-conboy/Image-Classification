d38c99e820d29274017f21f1658d2f97c5fb43dc
Carter Conboy <group64@elec377.queensu.ca>
2023-11-05 14:43:24 -0500

    Renamed Files

---
+++ net_vanilla.py
@@ -0,0 +1,98 @@
+#   Partial implementation of:
+#       Huang & Belongie, "Arbitrary Style Transfer in Real-Time with Adaptive Instance Normalization", arXiv:1703.06868v2, 30 July 2017
+#
+#   Adapted from:
+#       https://github.com/naoto0804/pytorch-AdaIN
+#
+import torch.nn.functional as F #introduce activation functions
+import torch.nn as nn
+import torch
+
+class encoder_decoder:
+    encoder = nn.Sequential(
+        nn.Conv2d(3, 3, (1, 1)),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(3, 64, (3, 3)),
+        nn.ReLU(),  # relu1-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, (3, 3)),
+        nn.ReLU(),  # relu1-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 128, (3, 3)),
+        nn.ReLU(),  # relu2-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, (3, 3)),
+        nn.ReLU(),  # relu2-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 256, (3, 3)),
+        nn.ReLU(),  # relu3-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-2
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-3
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-4
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 512, (3, 3)),
+        nn.ReLU(),  # relu4-1, this is the last layer used
+    )
+
+    frontend = nn.Sequential(
+        nn.Linear(8192, 4096),
+        nn.Linear(4096, 100),
+        nn.Softmax(),
+    )
+class CJNet(nn.Module):
+
+    def __init__(self, encoder, frontend=None):
+        super(CJNet, self).__init__()
+        self.encoder = encoder
+        self.frontend = frontend
+
+        # freeze encoder weights
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+        # need access to these intermediate encoder steps
+        # for the AdaIN computation
+        encoder_list = list(encoder.children())
+        self.encoder_stage_1 = nn.Sequential(*encoder_list[:4])  # input -> relu1_1
+        self.encoder_stage_2 = nn.Sequential(*encoder_list[4:11])  # relu1_1 -> relu2_1
+        self.encoder_stage_3 = nn.Sequential(*encoder_list[11:18])  # relu2_1 -> relu3_1
+        self.encoder_stage_4 = nn.Sequential(*encoder_list[18:31])  # relu3_1 -> relu4_1
+
+        #   if no decoder loaded, then initialize with random weights
+        if self.frontend == None:
+            # self.decoder = _decoder
+            self.frontend = encoder_decoder.frontend
+            self.init_decoder_weights(mean=0.0, std=0.01)
+
+        frontend_list = list(self.frontend.children())
+        self.front_end_stages = nn.Sequential(*frontend_list)
+
+    def init_decoder_weights(self, mean, std):
+        for param in self.frontend.parameters():
+            nn.init.normal_(param, mean=mean, std=std)
+
+    def encode(self, X):
+        relu1_1 = self.encoder_stage_1(X)
+        relu2_1 = self.encoder_stage_2(relu1_1)
+        relu3_1 = self.encoder_stage_3(relu2_1)
+        relu4_1 = self.encoder_stage_4(relu3_1)
+        return relu1_1, relu2_1, relu3_1, relu4_1
+    
+    def decode(self, X):
+        return self.front_end_stages(X)
+    
+    def forward(self, input):   
+        features = self.encode(input)[3]
+        features = torch.flatten(features)
+        inference = self.decode(features)
+        return inference
+       
---
+++ train_mod.py
@@ -0,0 +1,132 @@
+
+#import math
+import torchvision.transforms as transforms 
+from torchvision.utils import save_image
+import net_mod as net
+from torchvision.datasets import CIFAR100
+from torch.utils.data import DataLoader
+#from tensorboardX import SummaryWriter
+from torchvision import transforms
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+import torch
+import os
+import argparse
+
+#Argument parsing:
+parser = argparse.ArgumentParser()
+
+# training options
+parser.add_argument('-s', default='./experiments',
+                    help='Directory to save the model')
+parser.add_argument('-l', type=str, default='./frontendsaved100.pth')
+parser.add_argument('-lr', type=float, default=1e-5)
+parser.add_argument('-e', type=int, default=50)
+parser.add_argument('-b', type=int, default=1000)
+
+parser.add_argument('-cuda', type=str, default='Y')
+
+parser.add_argument('-p', type=str, default='loss100.png')
+args = parser.parse_args()
+
+
+epochs = args.e
+batchs = args.b
+if(args.cuda=='Y'):
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+
+train_transform = transforms.Compose([transforms.ToTensor()])
+train_set = CIFAR100('./data/', train=True, download=True, transform=train_transform)
+train_dataloader = DataLoader(train_set, batch_size=int(batchs), shuffle=True)
+
+encoder = net.encoder_decoder.encoder
+encoder.load_state_dict(torch.load("./encoder.pth", map_location=device))
+#frontend = net.encoder_decoder.frontend
+#frontend.load_state_dict(torch.load("./frontend1.pth", map_location=device))
+#model = net.CJNet(encoder, frontend)
+
+model = net.CJNet(encoder)
+model.train()
+model.to(device)
+
+optimizer = torch.optim.Adam(model.frontend.parameters(), lr=args.lr)
+loss_fn = torch.nn.CrossEntropyLoss()
+"""
+#Tensor to hold loss at each epoch
+losses_data = torch.zeros(epochs)
+losses_data = losses_data.to(device)
+for i in tqdm(range(epochs)):
+    print("epoch:" + str(i))
+    k = -1
+    for batch in train_dataloader:
+        #start recording loss
+        k+=1
+        print('i, batchNumber', i, k)
+        batch_loss = 0
+        for j in range(batchs):
+            image = batch[0][j]
+            image = image.to(device)
+            #call forward function
+            inference = model(image)
+            target = batch[1][j]
+            target = target.to(device)
+            
+            #Calculate loss
+            cross_entropy_loss = loss_fn(inference, target)
+            #Add to batch loss
+            batch_loss += cross_entropy_loss
+        #update model parameters based on loss
+        optimizer.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+        #update epoch loss
+        batch_loss = batch_loss/batchs
+        print(batch_loss)
+    if(i%10 == 0):
+        state_dict = model.frontend.state_dict()
+        torch.save(state_dict, 
+                   './frontend_iter_{:d}.pth'.format(i + 1))
+    losses_data[i] += batch_loss
+"""
+
+#Tensor to hold loss at each epoch
+losses_data = torch.zeros(epochs)
+losses_data = losses_data.to(torch.device(device))
+for i in tqdm(range(epochs)):
+    print("epoch:" + str(i))
+    k = -1
+    epoch_loss = 0
+    for batch in train_dataloader:
+        #start recording loss
+        k+=1
+        print('i, batchNumber', i, k)
+        for j in range(batchs):
+            image = batch[0]
+            image = image.to(device)
+            #call forward function
+            inference = model(image)
+            target = batch[1]
+            target = target.to(device)
+            
+            #Calculate loss
+            cross_entropy_loss = loss_fn(inference, target)
+            epoch_loss += cross_entropy_loss
+        #update model parameters based on loss
+        optimizer.zero_grad()
+        cross_entropy_loss.backward()
+        optimizer.step()
+        print(cross_entropy_loss)
+    losses_data[i] += epoch_loss
+
+state_dict = model.frontend.state_dict()
+decoder_state_dict_file = os.path.join(os.getcwd(), "/frontend.pth")
+torch.save(state_dict, decoder_state_dict_file)
+
+plt.plot(losses_data.cpu().detach().numpy())
+str
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.savefig("./loss.png")
